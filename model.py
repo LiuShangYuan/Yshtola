be301cb6ece4570269ce3323c922eae676da86da
muenn <532592399@qq.com>
2019-03-01 11:50:05 +0800

    descriminator; cnn-classifier based content extractor; TODO: autoencoder based content extractor; TODO: generator

--- model.py
+++ model.py
@@ -6,16 +6,35 @@ import numpy as np
 class DTN(object):
 
     def __init__(self, mode='train',
+                 embedding_size = 64,
                  vocab_size=10000,
-                 max_seq_len=50,
+                 max_seq_len=256,
+                 hidden_size=128,
+                 num_classes=2,
+                 num_filters=256,
+                 kernel_size=5,
+                 keep_prob=0.9,
                  learning_rate=0.0003,
                  rho=15.0):
         self.mode = mode
+
+        self.embedding_size = embedding_size
         self.vocab_size = vocab_size
         self.max_seq_len = max_seq_len
+        self.num_classes = num_classes
+
+        self.hidden_size = hidden_size
+        self.num_filters = num_filters
+        self.kernel_size = kernel_size
+        self.keep_prob = keep_prob
+
         self.learning_rate = learning_rate
 
         self.rho = rho
+
+        with tf.variable_scope('word_vector'):
+            with tf.device('/cpu:0'):
+                self.embedding = tf.get_variable('embedding', [self.vocab_size, self.embedding_size])
 
 
     def generator(self, inputs, reuse=False):
@@ -23,21 +42,89 @@ class DTN(object):
         return None
 
     def discriminator(self, texts, reuse=False):
-        # TODO: Implement a text classifier here
-        return None
+        with tf.device('/cpu:0'):
+            embedding_inputs = tf.nn.embedding_lookup(self.embedding, texts)
+
+        with tf.variable_scope('discriminator', reuse=reuse):
+
+            with tf.variable_scope('cnn'):
+                conv = tf.layers.conv1d(embedding_inputs, self.num_filters, self.kernel_size, name='conv')
+                gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
+
+            with tf.variable_scope('score'):
+                fc = tf.layers.dense(gmp, self.hidden_size, name='fc1')
+                fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+                fc = tf.nn.relu(fc)
+                fc = tf.layers.dense(fc, 2, name='fc2')
+
+        return fc
 
     def ae_extractor(self, texts, reuse=False):
+        # TODO: Firstly we implement a classifier here
+        # with tf.device('/cpu:0'):
+        #     # embedding = tf.get_variable('embedding', [self.vocab_size, self.embedding_size])
+        #     embedding_inputs = tf.nn.embedding_lookup(self.embedding, texts)
+        #
+        # with tf.variable_scope('content_extractor', reuse=reuse):
+        #
+        #     with tf.variable_scope('cnn'):
+        #         conv = tf.layers.conv1d(embedding_inputs, self.num_filters, self.kernel_size, name='conv')
+        #         gmp = tf.reduce_max(conv, reduction_indices=[1], name='gmp')
+        #
+        #     with tf.variable_scope('score'):
+        #         fc = tf.layers.dense(gmp, self.hidden_size, name='fc1')
+        #         fc = tf.contrib.layers.dropout(fc, self.keep_prob)
+        #         fc = tf.nn.relu(fc)
+        #
+        #         if self.mode == 'pretrain':
+        #             fc = tf.layers.dense(fc, self.num_classes, name='fc2')
+        #
+        #         return fc
+
         # TODO: Implement an autoencoder here
+        # with tf.device('/cpu:0'):
+        #     embedding_inputs = tf.nn.embedding_lookup(self.embedding, texts)
+        #
+        # with tf.variable_scope('content_extractor', reuse=reuse):
+        #
+        #     with tf.variable_scope('encoder'):
+        #         encoder_cell = tf.nn.rnn_cell.LSTMCell(self.hidden_size)
+        #
+        #         z_, enc_state = tf.contrib.rnn.static_rnn(encoder_cell,
+        #                                                   embedding_inputs,
+        #                                                   dtype=tf.float32)
+        #
+        #     with tf.variable_scope('decoder'):
+        #         dec_state = enc_state
+        #         dec_input_ = tf.ones(tf.shape(texts)[0], dtype=tf.int32)
+
         return None
 
     def build_model(self):
 
         if self.mode == 'pretrain':
-            self.src_texts = tf.placeholder(tf.int64, [None, self.max_seq_len], 'src_texts')        # batch_size x seq_len
+            self.texts = tf.placeholder(tf.int32, [None, self.max_seq_len], 'src_texts')        # batch_size x seq_len
+            self.labels = tf.placeholder(tf.int64, [None,], 'src_labels')
+
+            # logits and accuracy
+            self.logits = self.ae_extractor(self.texts)
+            self.pred = tf.argmax(self.logits, 1)
+            self.correct_pred = tf.equal(self.pred, self.labels)
+            self.accuracy = tf.reduce_mean(tf.cast(self.correct_pred, tf.float32))
+
+            # loss and train op
+            self.loss = slim.losses.sparse_softmax_cross_entropy(self.logits, self.labels)
+            self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
+            self.train_op = slim.learning.create_train_op(self.loss, self.optimizer)
+
+            # summary op
+            loss_summary = tf.summary.scalar('classification_loss', self.loss)
+            accuracy_summary = tf.summary.scalar('accuracy', self.accuracy)
+            self.summary_op = tf.summary.merge([loss_summary, accuracy_summary])
 
         elif self.mode == 'train':
-            self.src_texts = tf.placeholder(tf.int64, [None, self.max_seq_len], 'src_texts')
-            self.trg_texts = tf.placeholder(tf.int64, [None, self.max_seq_len], 'trg_texts')
+            self.src_texts = tf.placeholder(tf.int32, [None, self.max_seq_len], 'src_texts')
+            self.trg_texts = tf.placeholder(tf.int32, [None, self.max_seq_len], 'trg_texts')
 
             # source domain
             self.fx = self.ae_extractor(self.src_texts)
--- solver.py
+++ solver.py
@@ -6,6 +6,8 @@ import os
 import scipy.io
 import scipy.misc
 
+from tensorflow import keras
+dataset = keras.datasets.imdb
 
 class Solver(object):
 
@@ -33,14 +35,67 @@ class Solver(object):
         self.config = tf.ConfigProto()
         self.config.gpu_options.allow_growth = True
 
-    def load_src_texts(self):
-        pass
+    def load_src_texts(self, split='train'):
+        (train_data, train_labels), (test_data, test_labels) = dataset.load_data(num_words=10000, maxlen=self.model.max_seq_len)
 
-    def load_trg_texts(self):
+        train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                           value=0,
+                                                           padding='post',
+                                                           maxlen=self.model.max_seq_len)
+        test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                           value=0,
+                                                           padding='post',
+                                                           maxlen=self.model.max_seq_len)
+
+        if split == 'train':
+            print("TRAIN_DATA_LENS:", len(train_data))
+            return train_data, train_labels
+        elif split == 'test':
+            print("TEST_DATA_LENS:", len(test_data))
+            return test_data, test_labels
+        else:
+            print("Unknown split %s" % split)
+            return None, None
+
+    def load_trg_texts(self, split='train'):
         pass
 
     def pretrain(self):
-        pass
+        train_texts, train_labels = self.load_src_texts(split='train')
+        test_texts, test_labels = self.load_src_texts(split='test')
+
+        model = self.model
+        model.build_model()
+
+        with tf.Session(config=self.config) as sess:
+            tf.global_variables_initializer().run()
+            saver = tf.train.Saver()
+            summary_writer = tf.summary.FileWriter(logdir=self.log_dir, graph=tf.get_default_graph())
+
+            num_of_batches = train_texts.shape[0] // self.batch_size
+
+            for step in range(self.pretrain_iter+1):
+                i = step % num_of_batches
+                # if i == 0: shuffle
+                batch_texts = train_texts[i*self.batch_size: (i+1)*self.batch_size]
+                batch_labels = train_labels[i*self.batch_size: (i+1)*self.batch_size]
+                feed_dict = {model.texts: batch_texts, model.labels: batch_labels}
+                sess.run(model.train_op, feed_dict)
+
+                if (step + 1) % 10 == 0:
+                    summary, l, acc = sess.run([model.summary_op, model.loss, model.accuracy], feed_dict)
+                    # rand_idx
+                    test_acc, _ = sess.run(fetches=[model.accuracy, model.loss],
+                                           feed_dict={model.texts: test_texts,
+                                                      model.labels: test_labels})
+                    summary_writer.add_summary(summary, step)
+                    print('Step: [%d/%d]  loss: [%.6f]  train acc: [%.2f]  test acc: [%.2f]' \
+                          % (step+1, self.pretrain_iter, l, acc, test_acc))
+
+                if (step+1) % 1000 == 0:
+                    saver.save(sess, os.path.join(self.model_save_path, 'src_model'), global_step=step+1)
+                    print('src_model-%d saved...!' % (step+1))
+
 
     def train(self):
         pass
