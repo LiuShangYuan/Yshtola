99b8bfecb4dd934bd2a16e1b4cc0e9a06cdb75ac
muenn <532592399@qq.com>
2019-03-03 20:28:36 +0800

    update propro

--- prepro.py
+++ prepro.py
@@ -36,8 +36,8 @@ def zip2text():
 
 def split_dataset():
 
-    f_train = open('./data/amazon_train.txt', 'w')
-    f_test = open('./data/amazon_test.txt', 'w')
+    f_train = open('./data/amazon_train.json', 'w')
+    f_test = open('./data/amazon_test.json', 'w')
 
     d_true = []
     d_false = []
@@ -70,18 +70,8 @@ def split_dataset():
 
     print('%d samples in train_set and %d samples in test_set' % (len(d_train), len(d_test)))
 
-    for d in d_train:
-        f_train.write(d[0])
-        f_train.write('\t')
-        f_train.write(d[1])
-        f_train.write('\n')
-
-    for d in d_test:
-        f_test.write(d[0])
-        f_test.write('\t')
-        f_test.write(d[1])
-        f_test.write('\n')
-
+    json.dump(d_train, f_train)
+    json.dump(d_test, f_test)
 
 if __name__ == '__main__':
     zip2text()
