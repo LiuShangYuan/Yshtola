758a75b58724ed296be97f6aa360dcc4868dca30
muenn <532592399@qq.com>
2019-03-04 21:54:44 +0800

    add corpus

--- data/data_download.sh
+++ data/data_download.sh
@@ -1,1 +1,5 @@
-curl -o reviews_Books_5.json.gz http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Books_5.json.gz
+# curl -o reviews_Books_5.json.gz http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Books_5.json.gz
+
+# curl -o e2e-dataset.zip https://github.com/tuetschek/e2e-dataset/releases/download/v1.0.0/e2e-dataset.zip
+
+curl -o reviews_VideoGames_5.json.gz http://snap.stanford.edu/data/amazon/productGraph/categoryFiles/reviews_Video_Games_5.json.gz
